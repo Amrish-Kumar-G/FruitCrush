2efdac92fbf3c4902d7475c9ea521a4c70a4b769
BharathwajGoudBodige <bharathwajgoud6@gmail.com>
2022-04-05 20:55:25 +0530

    Login Validation Committed

--- FruitCrush/fruitcrushfrontend/src/app/components/login/login.component.ts
+++ FruitCrush/fruitcrushfrontend/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/classes/user';
 import { UserService } from 'src/app/service/user.service';
+import { AbstractControl, FormBuilder,  FormControl,  FormGroup,  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +9,39 @@ import { UserService } from 'src/app/service/user.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup =new FormGroup({
+    email:new FormControl(''),
+    password:new FormControl('')
+    })
+    title = 'validation';
   user:User=new User();
   tempuser:User=new User();
-  constructor(private userservice:UserService) { }
+  constructor(private userservice:UserService, private formBuilder: FormBuilder) { }
+  submitted = false;
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(40)
+        ]
+      ]
+    })
   }
+  get f(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    console.log(JSON.stringify(this.loginForm.value, null, 2));
+}
   authUser(){
     this.user.active=false;
     console.log(this.userservice.authuser(this.user).subscribe((data)=>{
